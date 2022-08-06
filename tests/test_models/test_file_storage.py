65a1b876054439e55f259ef5c0683356f99944b8
Maysummer <udojyk@gmail.com>
2022-08-06 14:25:07 -0700

    ,

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Test for file_storage"""
+import unittest
+from models import storage
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+import models.user import User
+
+
+class FileStorageTest(unittest.TestCase):
+    """File_storage test"""
+    def saveUser:
+        new_model = BaseModel()
+        new_model.name = "My_first"
+        new_model.save
+        all_obj = storage.all()
+        for obj in all_obj.keys():
+            del all_obj[obj]
+
+        self.assertIsEqual(new_model, 
